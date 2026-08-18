c03366b118dee085cd9037ed97e050ce0ce53d5e
vaishnaviKokate20 <vaishnavikokate20@gmail.com>
2026-08-18 10:49:01 +0530

    Capitalize Letter

---
+++ String.main/CapitalizeFirstLastLetter.java
@@ -0,0 +1,21 @@
+public class CapitalizeFirstLastLetter {
+    public static void main(String[] args) {
+        String str="khushi kokate";
+        char [] arr=str.toCharArray();
+         int start=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==' '){
+                int end=i-1;
+                arr[start]=Character.toUpperCase(arr[start]);
+                arr[end]=Character.toUpperCase(arr[end]);
+                start=i+1;
+            }
+        }
+        int end=arr.length-1;
+         arr[end]=Character.toUpperCase(arr[end]);
+
+        str=new String(arr);
+        System.out.println(str);
+    }
+    
+}
